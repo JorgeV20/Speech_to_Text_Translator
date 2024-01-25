b30549ef79fe0ce7c5da75a36376b064e0ae1fc8
JorgeV20 <jorg.v.2094@gmail.com>
2024-01-25 11:56:43 +0100

    new code

--- speech_to_text.py
+++ speech_to_text.py
@@ -2,19 +2,24 @@ import speech_recognition as sr
 from recording_audio import recording
 
 
-recording('test.wav')
 
-filename='test.wav'
+def get_audio(file_name):
 
-r=sr.Recognizer()
+    name=file_name +'.wav'
+
+    recording(name)
+
+    filename=name
+
+    r=sr.Recognizer()
 
 
-# open the file
-with sr.AudioFile(filename) as source:
-    # listen for the data (load audio to memory)
-    audio_data = r.record(source)
-    # recognize (convert from speech to text) /it is using Google Speech Recognition
-    text = r.recognize_google(audio_data)
-    
-    print(text)
+    # open the file
+    with sr.AudioFile(filename) as source:
+        # listen for the data (load audio to memory)
+        audio_data = r.record(source)
+        # recognize (convert from speech to text) /it is using Google Speech Recognition
+        text = r.recognize_google(audio_data)
+        
+        print(text)
 
